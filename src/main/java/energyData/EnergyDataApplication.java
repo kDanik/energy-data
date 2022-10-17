942acc5206347babdf587f171fc7ba8bcbc680ed
Daniil Kurachkin <86569730+kDanik@users.noreply.github.com>
2022-10-17 20:55:45 +0200

    initial commit, added standard spring boot application

---
+++ src/main/java/energyData/EnergyDataApplication.java
@@ -0,0 +1,11 @@
+package energyData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyDataApplication.class, args);
+    }
+}
