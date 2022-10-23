b299ba800b24e79e5dd4059a58bc1063d3008cce
Daniil Kurachkin <86569730+kDanik@users.noreply.github.com>
2022-10-23 13:44:24 +0200

    Replace legacy Date with LocalDate and add splitting of data fetching into smaller chuncks

--- src/main/java/energyData/EnergyDataRunner.java
+++ src/main/java/energyData/EnergyDataRunner.java
@@ -9,8 +9,7 @@ import energyData.service.query.service.EnergyDataQueryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
@@ -28,19 +27,11 @@ public class EnergyDataRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 2022);
-        cal.set(Calendar.MONTH, Calendar.AUGUST);
-        cal.set(Calendar.DAY_OF_MONTH, 10);
-        Date start = cal.getTime();
+        LocalDate startDate = LocalDate.of(2022, 2, 1);
+        LocalDate endDate = LocalDate.of(2022, 3, 1);
 
-        cal.set(Calendar.YEAR, 2022);
-        cal.set(Calendar.MONTH, Calendar.SEPTEMBER);
-        cal.set(Calendar.DAY_OF_MONTH, 1);
-        Date end = cal.getTime();
-
-        try{
-            List<String> testData = energyDataQueryService.fetchHourlyEnergyData(start, end);
+        try {
+            List<String> testData = energyDataQueryService.fetchHourlyEnergyData(startDate, endDate);
             List<EnergyData> energyData = energyDataParser.parseEnergyDataString(testData.get(0));
             System.out.println(energyData);
         } catch (EnergyDataQueryException | EnergyDataParsingException e) {
--- src/main/java/energyData/service/query/service/EnergyDataQueryService.java
+++ src/main/java/energyData/service/query/service/EnergyDataQueryService.java
@@ -2,7 +2,6 @@ package energyData.service.query.service;
 
 import energyData.service.query.exception.EnergyDataQueryException;
 import energyData.service.query.exception.TooBigTimePeriodException;
-import energyData.util.DateUtil;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
@@ -11,47 +10,80 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class EnergyDataQueryService {
     private final RestTemplate restTemplate;
 
-    private final DateFormat apiDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private final DateTimeFormatter apiDateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    // if time period is bigger than 28 days (one calendar month) the return data will have
+    // time interval of 1 day, instead of required time interval of 1 hour.
+    // 28 is obviously minimum possible month size.
+    private final int MAXIMUM_TIME_PERIOD_PER_FETCH = 28;
 
     public EnergyDataQueryService() {
         this.restTemplate = new RestTemplate();
     }
 
-    /* TODO the return data should be tested to check if it changes time intervals with bigger time period
-        (few month period returns data in 1 hour intervals, 1 year data return in... ???)
+    /**
+     * Fetches energy consumption and generation data for given time period and return it as String array,
+     * where each string is data for time period of 28 <= days. So time period of 100 days will return 4 strings in total (28 + 28 + 28 + 16).
+     * @param startDate startDate for fetching (result includes startDate)
+     * @param endDate end for fetching (result includes endDate)
+     * @return String array with request results
      */
-    public List<String> fetchHourlyEnergyData(Date startDate, Date endDate) {
-        // this method will split request into multiple and combine them to keep data intervals same
+    public List<String> fetchHourlyEnergyData(LocalDate startDate, LocalDate endDate) {
+        // to keep interval between energy data entries 1 hour this method will split fetching into request-chunks of 28 days each
         List<String> result = new ArrayList<>(10);
 
+        boolean timePeriodFullyIterated = false;
+        LocalDate chunkEndDate = startDate;
 
-        try {
-            result.add(fetchOneEnergyDataChunk(startDate, endDate));
-        } catch (URISyntaxException | TooBigTimePeriodException | RestClientException e) {
-            e.printStackTrace();
+        while (!timePeriodFullyIterated) {
+            chunkEndDate = chunkEndDate.plusDays(MAXIMUM_TIME_PERIOD_PER_FETCH);
 
-            throw new EnergyDataQueryException("Exception while trying to fetch energy data");
+            // the new chunkEndDate should never be after endDate. If it reaches endDate, or is after endDate, time period is fully iterated
+            if (chunkEndDate.isAfter(endDate)) {
+                chunkEndDate = endDate;
+                timePeriodFullyIterated = true;
+            } else {
+                if (chunkEndDate.equals(endDate)) {
+                    timePeriodFullyIterated = true;
+                }
+            }
+
+            try {
+                result.add(fetchOneEnergyDataChunk(startDate, chunkEndDate));
+            } catch (URISyntaxException | TooBigTimePeriodException | RestClientException e) {
+                e.printStackTrace();
+                throw new EnergyDataQueryException("Exception while trying to fetch energy data");
+            }
+
+            startDate = chunkEndDate;
         }
 
         return result;
     }
 
-    private String fetchOneEnergyDataChunk(Date startDate, Date endDate) throws URISyntaxException {
-        // if time period is bigger than 28 days (one calendar month) the return data will have
-        // time interval of 1 day, instead of required time interval of 1 hour
-        if (DateUtil.getDateDifferenceInDays(endDate, startDate) >= 28) throw new TooBigTimePeriodException("The time period for one energy data fetch can't be bigger than 28 days!");
+    /***
+     * Fetches one energy data chunk for given time period (maximum 28 days)
+     * @param startDate
+     * @param endDate
+     * @return one energy data chunk
+     * @throws URISyntaxException if request URI couldn't be created with given parameters
+     */
+    private String fetchOneEnergyDataChunk(LocalDate startDate, LocalDate endDate) throws URISyntaxException {
+        if (ChronoUnit.DAYS.between(startDate, endDate) > MAXIMUM_TIME_PERIOD_PER_FETCH) {
+            throw new TooBigTimePeriodException("The time period for one energy data fetch can't be bigger than 28 days! Input period is: " + ChronoUnit.DAYS.between(startDate, endDate));
+        }
 
         URI uri = buildServiceURI(startDate, endDate);
 
@@ -64,7 +96,7 @@ public class EnergyDataQueryService {
         return result.getBody().toString();
     }
 
-    private URI buildServiceURI(Date startDate, Date endDate) throws URISyntaxException {
+    private URI buildServiceURI(LocalDate startDate, LocalDate endDate) throws URISyntaxException {
         String formattedStartDate = apiDateFormat.format(startDate);
         String formattedEndDate = apiDateFormat.format(endDate);
 
